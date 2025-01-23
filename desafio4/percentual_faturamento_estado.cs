0c8a8debc8264181a32ac893781184498582e01f
GBMello33 <gabriel.mello@33robotics.com>
2025-01-23 17:59:19 -0300

    Desafio4

---
+++ desafio4/percentual_faturamento_estado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        var estados = new (string, double)[] {
+            ("SP", 67836.43),
+            ("RJ", 36678.66),
+            ("MG", 29229.88),
+            ("ES", 27165.48),
+            ("Outros", 19849.53)
+        };
+
+        double total = 0;
+        foreach (var estado in estados) total += estado.Item2;
+
+        foreach (var estado in estados) {
+            double percentual = (estado.Item2 / total) * 100;
+            Console.WriteLine($"{estado.Item1}: {percentual:F2}% do faturamento total.");
+        }
+    }
+}
